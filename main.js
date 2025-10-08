f5625c8d0872424c7cd29f6bff58c34609aae9d7
Sarah R. Vieira <sarahrdvieira@gmail.com>
2025-10-08 20:54:09 -0300

    Create gameboard object with arrays

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('hello :)');
+// Store gameboard as an array inside of gameboard object
+const Gameboard = (function () {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  // Create 2d array with 3 arrays for rows and 3 columns of elements
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i][j] = j;
+    }
+  }
+
+  const getBoard = () => board;
+
+  return { getBoard };
+})();
+
+console.log(Gameboard.getBoard());
